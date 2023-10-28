4e6ac827f739d4b156f0962b17f3825f74aaade3
Mega-Zinyz <109564254+Mega-Zinyz@users.noreply.github.com>
2023-10-28 09:37:29 +0700

    UTS 212102416
    28/10/2023

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "ecommerce", component: EcommerceComponent},
+  {path: "footer", component: FooterComponent},
+  {path: "header", component: HeaderComponent},
+  {path: "sidebar", component: SidebarComponent},
+  {path: "", redirectTo: "ecommerce", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/ecommerce/ecommerce.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ecommerce',
+  templateUrl: './ecommerce.component.html',
+  styleUrls: ['./ecommerce.component.css']
+})
+export class EcommerceComponent {
+  nama = 'Haggai Gershom Harnowo';
+  nim = '212102416';
+}
